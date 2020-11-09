a039c145fb6d017e631a0024c7d436f876b91d39
Derek Morrison <derek.morrison@oliveai.com>
2020-11-09 08:39:31 -0600

    Fix broken build from last merge

--- ldk/go/uiClient.go
+++ ldk/go/uiClient.go
@@ -8,13 +8,15 @@ import (
 	"github.com/open-olive/loop-development-kit/ldk/go/proto"
 )
 
+// UIClient is used to hook into UI events such as listening to search controls.
 type UIClient struct {
 	client  proto.UIClient
-	session LoopSession
+	session *Session
 }
 
+// ListenSearchbar allows you to listen to searchbar events.
 func (u *UIClient) ListenSearchbar(ctx context.Context, handler ListenSearchHandler) error {
-	msg := &proto.SearchbarStreamRequest{Session: u.session.toProto()}
+	msg := &proto.SearchbarStreamRequest{Session: u.session.ToProto()}
 	client, err := u.client.SearchbarStream(ctx, msg)
 	if err != nil {
 		return err
@@ -41,8 +43,9 @@ func (u *UIClient) ListenSearchbar(ctx context.Context, handler ListenSearchHand
 
 }
 
+// ListenGlobalSearch allows you to listen to global search (omnibar) events.
 func (u *UIClient) ListenGlobalSearch(ctx context.Context, handler ListenSearchHandler) error {
-	msg := &proto.GlobalSearchStreamRequest{Session: u.session.toProto()}
+	msg := &proto.GlobalSearchStreamRequest{Session: u.session.ToProto()}
 	client, err := u.client.GlobalSearchStream(ctx, msg)
 	if err != nil {
 		return err
