20bf915ae30b0541e54aa0dae86e3a2a71a2a5d7
Benjamin Brewer <ben.brewer@oliveai.com>
2020-12-14 14:53:12 -0500

    WIP - add headers to response and request for csharp

--- ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Network/NetworkClient.cs
+++ ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Network/NetworkClient.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Protobuf;
+using Google.Protobuf.Collections;
+using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using OliveHelpsLDK.Logging;
 using Proto;
@@ -29,6 +32,9 @@ namespace OliveHelpsLDK.Network
                 Method = request.Method,
                 Url = request.URL
             };
+
+            message.Headers.Add(request.Headers);
+
             return Client.HTTPRequestAsync(message, CreateOptions(cancellationToken)).ResponseAsync
                 .ContinueWith(LoggedParser<Task<HTTPResponseMsg>, HTTPResponse>(task => FromProto(task.Result)),
                     cancellationToken);
@@ -36,11 +42,33 @@ namespace OliveHelpsLDK.Network
 
         internal static HTTPResponse FromProto(HTTPResponseMsg response)
         {
-            return new HTTPResponse
+            var clientResponse = new HTTPResponse
             {
                 Data = response.ToByteArray(),
                 ResponseCode = checked((int) response.ResponseCode)
             };
+
+            clientResponse.Headers = ParseHeaders(response.Headers);
+
+            return clientResponse;
+        }
+
+        private static IDictionary<string, IList<string>> ParseHeaders(MapField<string, ListValue> headers)
+        {
+            var parsedHeaders = new Dictionary<string, IList<string>>();
+
+            foreach (var kvp in headers) parsedHeaders.Add(kvp.Key, ParseHeaderValues(kvp.Value));
+
+            return parsedHeaders;
+        }
+
+        private static IList<string> ParseHeaderValues(ListValue listValue)
+        {
+            var values = new List<string>();
+
+            foreach (var value in listValue.Values) values.Add(value.StringValue);
+
+            return values;
         }
     }
 }
