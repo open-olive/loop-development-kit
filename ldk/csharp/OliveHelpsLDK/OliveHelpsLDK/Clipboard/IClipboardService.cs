4fc746721fba11f05f819a4f7881e0613ac91988
richseviora <richard.seviora@gmail.com>
2020-12-15 10:02:28 -0800

    [SIDE-925] Document services

--- ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Clipboard/IClipboardService.cs
+++ ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Clipboard/IClipboardService.cs
@@ -3,11 +3,31 @@ using System.Threading.Tasks;
 
 namespace OliveHelpsLDK.Clipboard
 {
+    /// <summary>
+    /// Provides access to the contents of the Clipboard.
+    /// </summary>
     public interface IClipboardService
     {
+        /// <summary>
+        /// Reads the clipboard's contents.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns>A Task resolving with the string contents of the clipboard.</returns>
         Task<string> Read(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Writes the provided text to the clipboard.
+        /// </summary>
+        /// <param name="contents">The text to write to the clipboard.</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>A Task resolving when the request is completed.</returns>
         Task Write(string contents, CancellationToken cancellationToken = default);
 
+        /// <summary>
+        /// Creates a stream receiving updates when the clipboard's contents change.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         IStreamingCall<string> Stream(CancellationToken cancellationToken = default);
     }
 }
