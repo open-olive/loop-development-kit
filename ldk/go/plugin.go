bf3000976c25ccd9d6783decc8d150e0725a2ebe
Billy Murray <billy.murray@oliveai.com>
2020-12-18 14:58:27 -0500

    Set connection configuration when launching plugin

--- ldk/go/plugin.go
+++ ldk/go/plugin.go
@@ -1,12 +1,24 @@
 package ldk
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/open-olive/go-plugin"
 )
 
 // ServeLoopPlugin serves a specific loop plugin.
-func ServeLoopPlugin(logger *Logger, loop Loop) {
+func ServeLoopPlugin(logger *Logger, loop Loop) error {
+	currentDirectory, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return err
+	}
+
 	plugin.Serve(&plugin.ServeConfig{
+		ConnectionConfig: plugin.ConnectionConfig{
+			Network:       "unix",
+			UnixSocketDir: currentDirectory,
+		},
 		Logger:          logger.Logger(),
 		HandshakeConfig: Handshake,
 		Plugins: map[string]plugin.Plugin{
@@ -16,4 +28,6 @@ func ServeLoopPlugin(logger *Logger, loop Loop) {
 		},
 		GRPCServer: plugin.DefaultGRPCServer,
 	})
+
+	return nil
 }
