a53dfe49428d933f066550d611fc1d6f9afbcae6
richseviora <richard.seviora@gmail.com>
2020-12-21 16:14:21 -0800

    [SIDE-925] Documented Forms, Lists, and the Window Services

--- ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Whispers/Forms.cs
+++ ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Whispers/Forms.cs
@@ -4,22 +4,54 @@ using OliveHelpsLDK.Whispers.Forms.Outputs;
 // ReSharper disable once CheckNamespace
 namespace OliveHelpsLDK.Whispers.Forms
 {
-    
-    public interface IWhisperFormResponse {}
+    /// <summary>
+    /// Base class for Whisper Form responses. Implementations will either be <see cref="WhisperUpdate"/> if
+    /// the user has updated a value, or <see cref="WhisperResult"/> when the form has been submitted.
+    /// </summary>
+    public interface IWhisperFormResponse
+    {
+    }
 
+    /// <summary>
+    /// Represents an incremental field update. 
+    /// </summary>
     public struct WhisperUpdate : IWhisperFormResponse
     {
+        /// <summary>
+        /// The key of the field updated.
+        /// </summary>
         public string Key;
+
+        /// <summary>
+        /// The output value of the field updated.
+        /// </summary>
         public IBase Output;
     }
 
+    /// <summary>
+    /// Represents the final submission of the form.
+    /// </summary>
     public struct WhisperResult : IWhisperFormResponse
     {
+        /// <summary>
+        /// Whether the form was submitted (<c>true</c>), or rejected/dismissed (<c>false</c>).
+        /// TODO: Confirm that dismissed forms return false.
+        /// </summary>
         public bool Result;
 
+        /// <summary>
+        /// If the form was submitted.
+        /// </summary>
         public bool IsSubmitted => Result;
+
+        /// <summary>
+        /// If the form was rejected.
+        /// </summary>
         public bool IsRejected => !Result;
 
+        /// <summary>
+        /// A dictionary of the form keys and their outputs.
+        /// </summary>
         public IDictionary<string, IBase> Outputs;
     }
 }
--- ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Whispers/List.cs
+++ ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Whispers/List.cs
@@ -1,44 +1,130 @@
 namespace OliveHelpsLDK.Whispers.List
 {
+    /// <summary>
+    /// Base class for all List elements.
+    /// </summary>
     public abstract class ListBase
     {
+        /// <summary>
+        /// The field order. Must be at least zero. List entries are ordered by order, then by key. 
+        /// </summary>
         public uint Order { get; }
+
+        /// <summary>
+        /// Whether the field is visible when the whisper is expanded or not.
+        /// TODO: Confirm this is accurate.
+        /// </summary>
         public bool Extra { get; }
     }
 
+    /// <summary>
+    /// The style to apply to the entry.
+    /// </summary>
     public enum Style
     {
+        /// <summary>
+        /// Standard styling.
+        /// </summary>
         None,
+
+        /// <summary>
+        /// TODO: Find this out.
+        /// </summary>
         Success,
+
+        /// <summary>
+        /// TODO: Find this out.
+        /// </summary>
         Warn,
+
+        /// <summary>
+        /// TODO: Find this out.
+        /// </summary>
         Error
     }
 
+    /// <summary>
+    /// Defines how the entry is aligned.
+    /// </summary>
     public enum Align
     {
+        /// <summary>
+        /// Left aligned.
+        /// </summary>
         Left,
+
+        /// <summary>
+        /// Center aligned.
+        /// </summary>
         Center,
+
+        /// <summary>
+        /// Right aligned.
+        /// </summary>
         Right
     }
 
+    /// <summary>
+    /// Defines a label/value pair.
+    /// </summary>
     public class ListPair : ListBase
     {
+        /// <summary>
+        /// The Label presented.
+        /// </summary>
         public string Label { get; }
+
+        /// <summary>
+        /// The value presented as string.
+        /// </summary>
         public string Value { get; }
+
+        /// <summary>
+        /// The entry's style.
+        /// </summary>
         public Style Style { get; }
+
+        /// <summary>
+        /// Whether the field can be copied or not.
+        /// TODO: Confirm
+        /// </summary>
         public bool Copyable { get; }
     }
 
+    /// <summary>
+    /// A message entry.
+    /// </summary>
     public class ListMessage : ListBase
     {
+        /// <summary>
+        /// The header of the message.
+        /// </summary>
         public string Header { get; }
+
+        /// <summary>
+        /// The body of the message.
+        /// </summary>
         public string Body { get; }
+
+        /// <summary>
+        /// The style the message is presented in.
+        /// </summary>
         public Style Style { get; }
+
+        /// <summary>
+        /// The alignment of the message.
+        /// </summary>
         public Align Align { get; }
     }
 
+    /// <summary>
+    /// A divider entry.
+    /// </summary>
     public class ListDivider : ListBase
     {
+        /// <summary>
+        /// The style of the divider.
+        /// </summary>
         public Style Style { get; }
     }
 }
