9375e7fa7a58b4de6db310be997a45dbf009a45d
richseviora <richard.seviora@gmail.com>
2021-01-06 09:17:53 -0800

    [SIDE-1154] Implement Filesystem changes and File class

--- ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Filesystem/IFilesystemService.cs
+++ ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/Filesystem/IFilesystemService.cs
@@ -10,15 +10,60 @@ namespace OliveHelpsLDK.Filesystem
         Task<IList<FileInfo>> QueryDirectory(string directoryPath, CancellationToken cancellationToken = default);
         IStreamingCall<FileEvent> StreamDirectory(string directoryPath, CancellationToken cancellationToken = default);
         IStreamingCall<FileEvent> StreamFileInfo(string filePath, CancellationToken cancellationToken = default);
+
+        Task MakeDirectory(MakeDirectoryParameters parameters, CancellationToken cancellationToken = default);
+
+        Task Copy(MoveOrCopyFileParameters parameters, CancellationToken cancellationToken = default);
+
+        Task Move(MoveOrCopyFileParameters parameters, CancellationToken cancellationToken = default);
+
+        Task Remove(RemoveParameters parameters, CancellationToken cancellationToken = default);
+
+        Task<IFile> OpenFile(string path, CancellationToken cancellationToken = default);
+
+        Task<IFile> CreateFile(string path, CancellationToken cancellationToken = default);
+    }
+
+    public interface IFile
+    {
+        Task<int> Write(byte[] data);
+
+        Task<byte[]> Read();
+
+        Task Close();
+
+        Task ChangePermissions(int permission);
+
+        Task ChangeOwnership(int userId, int groupId);
+
+        Task<FileInfo> FileInfo();
+    }
+
+    public struct MakeDirectoryParameters
+    {
+        public string Path;
+        public uint Permissions;
+    }
+
+    public struct MoveOrCopyFileParameters
+    {
+        public string Source;
+        public string Destination;
+    }
+
+    public struct RemoveParameters
+    {
+        public string Path;
+        public bool Recursive;
     }
 
     public struct FileInfo
     {
-        public string Name;
-        public long Size;
-        public uint Mode;
-        public DateTime Updated;
-        public bool IsDirectory;
+        public string Name { get; set; }
+        public long Size { get; set; }
+        public uint Mode { get; set; }
+        public DateTime Updated { get; set; }
+        public bool IsDirectory { get; set; }
     }
 
     public enum FileAction
