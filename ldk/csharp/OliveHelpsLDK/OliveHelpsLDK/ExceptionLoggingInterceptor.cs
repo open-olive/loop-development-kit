b505ca6bffa8c3c0a16de0fc68fb72176359a5f0
Benjamin Brewer <ben.brewer@oliveai.com>
2021-01-08 11:20:15 -0500

    add c# documentation

--- ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/ExceptionLoggingInterceptor.cs
+++ ldk/csharp/OliveHelpsLDK/OliveHelpsLDK/ExceptionLoggingInterceptor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ using OliveHelpsLDK.Logging;
 
 namespace OliveHelpsLDK
 {
+    /// <summary>
+    /// The ExceptionLoggingInterceptor extension of an Interceptor logs and passes along any errors occurring in asynchronous gRPC client calls
+    /// </summary>
     public class ExceptionLoggingInterceptor : Interceptor
     {
         private readonly ILogger _logger;
@@ -16,6 +20,13 @@ namespace OliveHelpsLDK
             _logger = logger;
         }
 
+        /// <summary>
+        /// Makes an asynchronous request for a single value via gRPC and logs any errors that occur.
+        /// </summary>
+        /// <param name="request">The gRPC request message.</param>
+        /// <param name="context">The gRPC context (service, method, etc.).</param>
+        /// <param name="continuation">The actual client method implementation function.</param>
+        /// <returns>A wrapped asynchronous call.</returns>
         public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(TRequest request,
             ClientInterceptorContext<TRequest, TResponse> context,
             AsyncUnaryCallContinuation<TRequest, TResponse> continuation)
@@ -28,6 +39,13 @@ namespace OliveHelpsLDK
             return WrapCall(call, logger, context.Options.CancellationToken);
         }
 
+        /// <summary>
+        /// Makes an asynchronous request for a stream of values via gRPC and logs any errors that occur.
+        /// </summary>
+        /// <param name="request">The gRPC request message.</param>
+        /// <param name="context">The gRPC context (service, method, etc.).</param>
+        /// <param name="continuation">The actual client method implementation function.</param>
+        /// <returns>A wrapped asynchronous call.</returns>
         public override AsyncServerStreamingCall<TResponse> AsyncServerStreamingCall<TRequest, TResponse>(
             TRequest request,
             ClientInterceptorContext<TRequest, TResponse> context,
@@ -41,6 +59,12 @@ namespace OliveHelpsLDK
             return WrapCall(call, logger);
         }
 
+        /// <summary>
+        /// Makes an asynchronous connection for a read and write stream via gRPC and logs any errors that occur.
+        /// </summary>
+        /// <param name="context">The gRPC context (service, method, etc.).</param>
+        /// <param name="continuation">The actual client method implementation function.</param>
+        /// <returns>A wrapped asynchronous call.</returns>
         public override AsyncDuplexStreamingCall<TRequest, TResponse> AsyncDuplexStreamingCall<TRequest, TResponse>(ClientInterceptorContext<TRequest, TResponse> context,
             AsyncDuplexStreamingCallContinuation<TRequest, TResponse> continuation)
         {
@@ -52,6 +76,13 @@ namespace OliveHelpsLDK
             return WrapCall(call, logger, context.Options.CancellationToken);
         }
 
+        /// <summary>
+        /// Wraps a gRPC call object in exception logging.
+        /// </summary>
+        /// <param name="call">A call which will have any internal Tasks wrapped in logging.</param>
+        /// <param name="logger">A logger to use for logging exceptions in Tasks.</param>
+        /// <param name="token">A cancellation token to wire into the async logging continuations.</param>
+        /// <returns>A wrapped asynchronous call.</returns>
         private static AsyncUnaryCall<TResponse> WrapCall<TResponse>(AsyncUnaryCall<TResponse> call, ILogger logger,
             CancellationToken token)
         {
@@ -59,6 +90,12 @@ namespace OliveHelpsLDK
                 call.ResponseHeadersAsync, call.GetStatus, call.GetTrailers, call.Dispose);
         }
 
+        /// <summary>
+        /// Wraps a gRPC call object in exception logging.
+        /// </summary>
+        /// <param name="call">A call which will have any internal Tasks wrapped in logging.</param>
+        /// <param name="logger">A logger to use for logging exceptions in Tasks.</param>
+        /// <returns>A wrapped asynchronous call.</returns>
         private static AsyncServerStreamingCall<TResponse> WrapCall<TResponse>(AsyncServerStreamingCall<TResponse> call,
             ILogger logger)
         {
@@ -66,6 +103,13 @@ namespace OliveHelpsLDK
                 call.ResponseHeadersAsync, call.GetStatus, call.GetTrailers, call.Dispose);
         }
         
+        /// <summary>
+        /// Wraps a gRPC call object in exception logging.
+        /// </summary>
+        /// <param name="call">A call which will have any internal Tasks wrapped in logging.</param>
+        /// <param name="logger">A logger to use for logging exceptions in Tasks.</param>
+        /// <param name="token">A cancellation token to wire into the async logging continuations.</param>
+        /// <returns>A wrapped asynchronous call.</returns>
         private static AsyncDuplexStreamingCall<TRequest, TResponse> WrapCall<TRequest, TResponse>(AsyncDuplexStreamingCall<TRequest, TResponse> call,
             ILogger logger, CancellationToken token)
         {
@@ -73,50 +117,85 @@ namespace OliveHelpsLDK
                 WrapStreamHandler(call.ResponseStream, logger), call.ResponseHeadersAsync, call.GetStatus, call.GetTrailers, call.Dispose);
         }
 
+        /// <summary>
+        /// Adds an exception logging continuation to a task.
+        /// </summary>
+        /// <remarks>This is where the bulk of the exception handling and logging work actually occurs.</remarks>
+        /// <param name="task">A task, to which an exception logging continuation will be added.</param>
+        /// <param name="logger">The logger used in exception handling.</param>
+        /// <param name="token">A cancellation token to attach to the logging continuation.</param>
+        /// <returns>The exception logging continuation.</returns>
         private static Task<TResponse> WrapContinueHandler<TResponse>(Task<TResponse> task, ILogger logger,
             CancellationToken token)
         {
             return task.ContinueWith(action =>
             {
                 if (action.Exception == null) return action.Result;
-                token.Register(() => throw action.Exception);
-                action.Exception.Handle(exception =>
-                {
-                    logger.Error("Client exception", exception);
-                    return true;
-                });
-
+                HandleException(action.Exception, logger, token);
                 return action.Result;
             }, token, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current);
         }
         
+        /// <summary>
+        /// Adds an exception logging continuation to a task.
+        /// </summary>
+        /// <remarks>Non-generic version of this function.</remarks>
         private static Task WrapContinueHandler(Task task, ILogger logger,
             CancellationToken token)
         {
             return task.ContinueWith(action =>
             {
                 if (action.Exception == null) return;
-                token.Register(() => throw action.Exception);
-                action.Exception.Handle(exception =>
+                HandleException(action.Exception, logger, token);
+            }, token, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current);
+        }
+
+        /// <summary>
+        /// Handles and logs an exception.
+        /// </summary>
+        /// <param name="aex">A Task's aggregate exception.</param>
+        /// <param name="logger">The logger used in exception handling.</param>
+        /// <param name="token">A cancellation token to which this will register a re-throw.</param>
+        private static void HandleException(AggregateException aex, ILogger logger, CancellationToken token)
+        {
+                token.Register(() => throw aex);
+                aex.Handle(exception =>
                 {
                     logger.Error("Client exception", exception);
                     return true;
                 });
-            }, token, TaskContinuationOptions.OnlyOnFaulted, TaskScheduler.Current);
         }
 
+        /// <summary>
+        /// Wraps a client read stream with a logging wrapper class.
+        /// </summary>
+        /// <param name="call">A stream reader.</param>
+        /// <param name="logger">A logger to use for logging exceptions in Tasks.</param>
+        /// <returns>A wrapped stream reader.</returns>
         private static IAsyncStreamReader<TResponse> WrapStreamHandler<TResponse>(IAsyncStreamReader<TResponse> call,
             ILogger logger)
         {
             return new AsyncStreamReaderWrapper<TResponse>(call, logger);
         }
-        
+
+        /// <summary>
+        /// Wraps a client write stream with a logging wrapper class.
+        /// </summary>
+        /// <param name="call">A stream writer.</param>
+        /// <param name="logger">A logger to use for logging exceptions in Tasks.</param>
+        /// <param name="token">A cancellation token to add to any wrapped Tasks.</param>
+        /// <returns>A wrapped stream writer.</returns>
         private static IClientStreamWriter<TRequest> WrapStreamHandler<TRequest>(IClientStreamWriter<TRequest> call,
             ILogger logger, CancellationToken token)
         {
             return new ClientStreamWriterWrapper<TRequest>(call, logger, token);
         }
 
+        /// <summary>
+        /// Extracts logging fields from the gRPC invocation context.
+        /// </summary>
+        /// <param name="context">An invocation context.</param>
+        /// <returns>A dictionary of fields.</returns>
         private static IDictionary<string, object> FieldsFromContext<TRequest, TResponse>(
             ClientInterceptorContext<TRequest, TResponse> context) where TResponse : class where TRequest : class
         {
@@ -127,6 +206,9 @@ namespace OliveHelpsLDK
             };
         }
 
+        /// <summary>
+        /// Wraps a client read stream in exception logging.
+        /// </summary>
         private class AsyncStreamReaderWrapper<TResponse> : IAsyncStreamReader<TResponse>
         {
             private readonly ILogger _logger;
@@ -138,8 +220,16 @@ namespace OliveHelpsLDK
                 _logger = logger;
             }
 
+            /// <summary>
+            /// Gets the current item in the read stream.
+            /// </summary>
+            /// <returns>The current item in the read stream.</returns>
             public TResponse Current => _wrapped.Current;
 
+            /// <summary>
+            /// Requests the next item in the read stream.
+            /// </summary>
+            /// <returns>A Task with a boolean representing a successful or failed move.</returns>
             public Task<bool> MoveNext(CancellationToken token)
             {
                 var moved = _wrapped.MoveNext(token);
@@ -147,6 +237,9 @@ namespace OliveHelpsLDK
                 return WrapContinueHandler(moved, _logger, token);
             }
         }
+        /// <summary>
+        /// Wraps a client write stream in exception logging.
+        /// </summary>
         private class ClientStreamWriterWrapper<TRequest> : IClientStreamWriter<TRequest>
         {
             private readonly ILogger _logger;
@@ -160,6 +253,10 @@ namespace OliveHelpsLDK
                 _token = token;
             }
 
+            /// <summary>
+            /// Writes an item to the write stream.
+            /// </summary>
+            /// <returns>A void Task to await.</returns>
             public Task WriteAsync(TRequest message)
             {
                 var written = _wrapped.WriteAsync(message);
@@ -167,12 +264,19 @@ namespace OliveHelpsLDK
                 return WrapContinueHandler(written, _logger, _token);
             }
 
+            /// <summary>
+            /// Sets or gets the stream writing options.
+            /// </summary>
             WriteOptions IAsyncStreamWriter<TRequest>.WriteOptions
             {
                 get => _wrapped.WriteOptions;
                 set => _wrapped.WriteOptions = value;
             }
 
+            /// <summary>
+            /// Ends the write stream.
+            /// </summary>
+            /// <returns>A void Task to await.</returns>
             public Task CompleteAsync()
             {
                 var completed = _wrapped.CompleteAsync();
