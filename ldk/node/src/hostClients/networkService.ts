8038604ad5915a5e61413308b8cedaca4b3b4153
Brett Mugglin <brett.mugglin@oliveai.com>
2021-01-12 19:17:06 -0800

    SIDE-1194 Update documentationbased on feedback from adaptrack

--- ldk/node/src/hostClients/networkService.ts
+++ ldk/node/src/hostClients/networkService.ts
@@ -8,7 +8,15 @@ export interface HttpRequest {
 
 export interface HttpResponse {
   statusCode: number;
-  data: string | Uint8Array;
+  /**
+   * The HTTP response as a byte array. To decode into a UTF-8 string you can:
+   * ```
+   * import * as util from 'util';
+   * ...
+   * var string = new TextDecoder("utf-8").decode(uint8array);
+   * ```
+   */
+  data: Uint8Array;
   headers: Record<string, Array<string>>;
 }
 
