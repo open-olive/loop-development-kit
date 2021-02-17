665b99990fe863157c19a7ad737673518b0e5840
Brett Mugglin <brett.mugglin@oliveai.com>
2021-02-17 15:01:10 -0800

    SIDE-1195 Remove old consolidated example. Write / read from file

--- ldk/node/examples/consolidatedExample/src/index.ts
+++ ldk/node/examples/consolidatedExample/src/index.ts
@@ -8,7 +8,6 @@ import {
 } from '../../../dist/hostClients/whisperService';
 import { StoppableStream } from '../../../dist/hostClients/stoppables';
 import formConfig from './formConfig';
-// import formConfig from './formConfig';
 
 const logger = new Logger('olive-helps-node-example');
 
@@ -27,8 +26,11 @@ class ClipboardLoop implements Loop {
         ctrl: true,
       },
     };
+    const decoder = new TextDecoder();
+    const encoder = new TextEncoder();
 
     try {
+      host.fileSystem.createFile('./data.txt');
       this.host.keyboard.streamHotKey(hotkeys, (error, keyboardResponse) => {
         if (error) {
           // Error found
@@ -47,12 +49,43 @@ class ClipboardLoop implements Loop {
           const submitEvent = formResponse as WhisperFormSubmitEvent;
 
           if (updateEvent.type === 'update') {
-            logger.info('update Occurred');
-            logger.info(JSON.stringify(updateEvent));
+            // do nothing for now
+            // logger.info('update Occurred');
+            // logger.info(JSON.stringify(updateEvent));
           } else if (submitEvent.type === 'submit' && submitEvent.submitted) {
-            logger.info('submit occurred');
-            logger.info(JSON.stringify(submitEvent));
-            // form.stop();
+            if (this.validateForm(submitEvent) && submitEvent.outputs.email) {
+              const readHandle = host.fileSystem.openFile('./data.txt');
+
+              readHandle
+                .read()
+                .then((fileContents) => {
+                  const file = decoder.decode(fileContents);
+                  logger.info(`${file === ''}`);
+                  const fileObj = file === '' ? {} : JSON.parse(file);
+                  fileObj[
+                    submitEvent.outputs.email.toString()
+                  ] = JSON.stringify(submitEvent.outputs);
+                  logger.info(JSON.stringify(fileObj));
+
+                  const writeHandle = host.fileSystem.openFile('./data.txt');
+                  return writeHandle.write(
+                    encoder.encode(JSON.stringify(fileObj)),
+                  );
+                })
+                .then(() => {
+                  logger.info('success');
+                })
+                .catch((err) => {
+                  logger.error(err);
+                });
+            } else {
+              logger.info('fail');
+              host.whisper.markdownWhisper({
+                label: 'There was an issue with the form data',
+                markdown:
+                  'Please make sure that DOB is in the format MM/DD/YYYY. Please retry data entry.',
+              });
+            }
           } else {
             logger.info('???');
           }
@@ -61,6 +94,15 @@ class ClipboardLoop implements Loop {
     } catch (e) {
       logger.error('Error Streaming', 'error', e.toString());
     }
+  }
+
+  validateForm(submitEvent: WhisperFormSubmitEvent): boolean {
+    // TODO: Probably want to regex the email too.
+    const dateRegex = new RegExp(
+      '^(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.](19|20)\\d\\d$',
+    );
+
+    return dateRegex.test(submitEvent.outputs.dob.toString());
   }
 
   stop(): void {
