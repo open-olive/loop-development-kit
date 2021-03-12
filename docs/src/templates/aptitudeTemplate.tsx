46a00533d3c6f1350d40e1bfdc6affce768f5e26
Rich Seviora <richard.seviora@gmail.com>
2021-03-12 10:45:04 -0800

    [SIDE-1196] Fix aptitude layout display

--- docs/src/templates/aptitudeTemplate.tsx
+++ docs/src/templates/aptitudeTemplate.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { PageProps } from 'gatsby';
-import styles from './aptitudeTemplate.module.scss';
 import { Aptitude } from '../components/aptitudes/aptitude';
 import { aptitudes } from '../components/aptitudes/aptitudeData';
-import { Menu } from '../components/menu/menu';
+import { StandardLayout } from './standard-layout';
 
 interface TemplateProps {
   markdownRemark: {
@@ -20,16 +19,10 @@ interface TemplatePageContext {
 }
 
 export default function Template(props: PageProps<TemplateProps, TemplatePageContext>) {
-  const aptitudeData = Object.values(aptitudes);
   const aptitudeId = props.pageContext.aptitudeId;
   return (
-    <>
-      <div className={styles.layout}>
-        <Menu currentPath={props.path}  />
-        <div className={styles.content}>
-          <Aptitude {...aptitudes[aptitudeId]} />
-        </div>
-      </div>
-    </>
+    <StandardLayout path={props.path}>
+      <Aptitude {...aptitudes[aptitudeId]} />
+    </StandardLayout>
   );
 }
