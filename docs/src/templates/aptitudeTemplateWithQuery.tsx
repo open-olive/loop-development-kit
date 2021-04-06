ee5837b98db37b70bd15f4cf1b18b3e19eacbc9f
Rich Seviora <richard.seviora@gmail.com>
2021-04-06 12:03:05 -0700

    [SIDE-1938] Combine aptitude data sources in menu

--- docs/src/templates/aptitudeTemplateWithQuery.tsx
+++ docs/src/templates/aptitudeTemplateWithQuery.tsx
@@ -1,51 +1,16 @@
 import React from 'react';
 import { graphql, PageProps } from 'gatsby';
 import { Aptitude } from '../components/aptitudes/aptitude';
-import { aptitudes, IAptitudeData } from '../components/aptitudes/aptitudeData';
 import { StandardLayout } from './standard-layout';
+import { getAptitudeDataFromQuery, TemplateProps } from '../queries';
 
-interface AptitudeMarkdown {
-  html: string;
-  frontmatter: {
-    name: string;
-    links_go: string | undefined;
-    links_node: string | undefined;
-  };
-}
-
-interface TemplateProps {
-  aptitude: {
-    internalName: string;
-    markdown: AptitudeMarkdown;
-    capabilities: {
-      markdown: AptitudeMarkdown;
-    }[];
-  };
-}
-
-interface TemplatePageContext {
+export interface TemplatePageContext {
   aptitudeId: string;
 }
 
 export default function Template(props: PageProps<TemplateProps, TemplatePageContext>) {
-  const data = props.data.aptitude;
-  const aptitudeFrontMatter = data.markdown.frontmatter;
-  const aptitudeData: IAptitudeData = {
-    name: aptitudeFrontMatter.name,
-    description: data.markdown.html,
-    links: {
-      node: aptitudeFrontMatter.links_node,
-      go: aptitudeFrontMatter.links_go,
-    },
-    capabilities: data.capabilities.map((capability) => ({
-      name: capability.markdown.frontmatter.name,
-      description: capability.markdown.html,
-      links: {
-        node: capability.markdown.frontmatter.links_node,
-        go: capability.markdown.frontmatter.links_go,
-      },
-    })),
-  };
+  const dataProp = props.data;
+  const aptitudeData = getAptitudeDataFromQuery(dataProp.aptitude);
   return (
     <StandardLayout path={props.path}>
       <Aptitude {...aptitudeData} />
