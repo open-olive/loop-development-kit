e676f34c84b56d7fd0207b4b870056eb23f610d8
Brett Mugglin <brett.mugglin@oliveai.com>
2021-04-13 15:13:16 -0700

    SIDE-2017 make changes from PR

--- ldk/goja/src/ui/index.ts
+++ ldk/goja/src/ui/index.ts
@@ -2,22 +2,25 @@
  * The UI aptitude gives access to the Olive helps search bar
  */
 export interface UI {
+  /**
+   * Creates a stream receiving updates whenever the user enters a search in the Olive Helps Searchbar.
+   *
+   * @param callback - The callback function called when an update to the searchbar occurs.
+   */
   listenSearchbar(cb: (val: string) => void): void;
+
+  /**
+   * Creates a stream receiving updates whenever the user enters a search in the Olive Helps Global Search.
+   *
+   * @param callback - The callback function called when an update to the global searchbar occurs.
+   */
   listenGlobalSearch(cb: (val: string) => void): void;
 }
 
-/**
- * Creates a stream receiving updates whenever the user enters a search in the Olive Helps Searchbar.
- * @param callback: The callback function that receives updates.
- */
 function listenSearchbar(callback: (val: string) => void): void {
   return oliveHelps.ui.listenSearchbar(callback);
 }
 
-/**
- * Creates a stream receiving updates whenever the user enters a search in the Olive Helps Global Search.
- * @param callback: The callback function that receives updates.
- */
 function listenGlobalSearch(callback: (val: string) => void): void {
   return oliveHelps.ui.listenGlobalSearch(callback);
 }
