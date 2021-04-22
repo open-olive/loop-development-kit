faf5d0e8ec8329ec0021781a0a8903879b63fcd7
Ruslan Guryanov <ruslan.guryanov@oliveai.com>
2021-04-22 12:12:14 -0400

    SIDE-2096: RG - adding comment for the ldk metadata version

--- ldk/goja/src/webpack/generate-banner.ts
+++ ldk/goja/src/webpack/generate-banner.ts
@@ -1,5 +1,8 @@
 export function generateMetadata(): string {
   const json = JSON.stringify({
+    /**
+     * referencing the ldk dependency version from loop package file, so it could be included as metadata of the minimized loop
+     */
     ldkVersion: process.env.npm_package_dependencies__oliveai_ldk,
   });
   return Buffer.from(json).toString('base64');
