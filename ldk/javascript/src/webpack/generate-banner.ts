bb59fc0651b292304a43b007a8cf4bdff97fff11
Cory Shields <cory.shields@oliveai.com>
2021-04-29 10:43:08 -0400

    SIDE-2121 Building permissions and throwing errors preview

--- ldk/javascript/src/webpack/generate-banner.ts
+++ ldk/javascript/src/webpack/generate-banner.ts
@@ -1,9 +1,9 @@
 export function generateMetadata(loopPackage: any): string {
   const json = JSON.stringify({
     ldkVersion: '0.1.0',
-    urlPermissions: loopPackage.permissions.urlPermissions,
-    filesystemPermissions: loopPackage.permissions.filesystemPermissions,
-    aptitudePermissions: loopPackage.permissions.aptitudePermissions
+    urlPermissions: buildUrlPermissions(loopPackage.permissions),
+    filesystemPermissions: buildFilesystemPermissions(loopPackage.permissions),
+    aptitudePermissions: buildAptitudePermissions(loopPackage.permissions)
   });
   return Buffer.from(json).toString('base64');
 }
@@ -16,4 +16,37 @@ export function generateBanner(loopPackage: any): string {
   ---END-LOOP-JSON-BASE64---
   */
  `;
+}
+
+function buildUrlPermissions(permissions: any): string {
+  if (isPermissionDefined(permissions.urlPermissions)) {
+    throw new Error("No permission declaration found for URLs. Add Loop permissions for URLs.");
+  } else {
+    return permissions.urlPermissions;
+  }
+}
+
+function buildFilesystemPermissions(permissions: any): string {
+  if (isPermissionDefined(permissions.filesystemPermissions)) {
+    throw new Error("No permission declaration found for filesystem. Add Loop mermissions for Filesystem.");
+  } else {
+    return permissions.filesystemPermissions;
+  }
+}
+
+function buildAptitudePermissions(permissions: any): string {
+  if (isPermissionDefined(permissions.aptitudePermissions)) {
+    throw new Error("No permission declaration found for aptitudes. Add Loop mermissions for aptitudes.");
+  } else {
+    return permissions.aptitudePermissions;
+  } 
+}
+
+function isPermissionDefined(permission: any): boolean {
+  if (permission === "" || 
+      permission === undefined) {
+    return true;
+  } else {
+    return false;
+  }
 }
