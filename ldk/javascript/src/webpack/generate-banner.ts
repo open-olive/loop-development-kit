80c71b6c5ed2f2e277573fb61ace9b3f89752163
Cory Shields <cory.shields@oliveai.com>
2021-04-29 18:00:44 -0400

    SIDE-2121: Modified json encoding structure

--- ldk/javascript/src/webpack/generate-banner.ts
+++ ldk/javascript/src/webpack/generate-banner.ts
@@ -1,4 +1,4 @@
-import { LdkPermissions, LdkSettings } from './ldk-settings';
+import { LdkAptitude, LdkFilesystem, LdkNetwork, LdkPermissions, LdkSettings } from './ldk-settings';
 
 function isPermissionUndefined(permission: string[]): boolean {
   if (permission === undefined || 
@@ -8,33 +8,37 @@ function isPermissionUndefined(permission: string[]): boolean {
   return false;
 }
 
-function buildUrlPermissions(ldkPermissions: LdkPermissions): string[] {
-  if (isPermissionUndefined(ldkPermissions.url)) {
-    throw new Error("No permission declaration found for URLs. Add Loop permissions for URLs.");
-  } else {
-    return ldkPermissions.url;
-  }
+function buildNetworkPermissions(ldkPermissions: LdkPermissions): LdkNetwork {
+  return ldkPermissions.network;
+  // TODO: Error handling
+  //   throw new Error("No permission declaration found for URLs. Add Loop permissions for URLs.");
 }
 
-function buildFilesystemPermissions(ldkPermissions: LdkPermissions): string[] {
-  if (isPermissionUndefined(ldkPermissions.filesystem)) {
-    throw new Error("No permission declaration found for filesystem. Add Loop permissions for Filesystem.");
-  } else {
-    return ldkPermissions.filesystem;
-  }
+function buildFilesystemPermissions(ldkPermissions: LdkPermissions): LdkFilesystem {
+  return ldkPermissions.filesystem;
+  // TODO: Error handling
+  //   throw new Error("No permission declaration found for filesystem. Add Loop permissions for Filesystem.");
 }
 
-function buildAptitudePermissions(permissions: LdkPermissions): string[] {
-    return permissions.aptitude;
+function validateAptitudePermissions(ldkPermissions: LdkAptitude): LdkAptitude {
+  return ldkPermissions;
+  // TODO: Error handling || return nil
 }
 
 export function generateMetadata(ldkSettings: LdkSettings): string {
   const json = JSON.stringify({
     ldkVersion: '0.1.0',
     permissions: {
-      urlPermissions: buildUrlPermissions(ldkSettings.ldk.permissions),
-      filesystemPermissions: buildFilesystemPermissions(ldkSettings.ldk.permissions),
-      aptitudePermissions: buildAptitudePermissions(ldkSettings.ldk.permissions)
+      clipboard: ldkSettings.ldk.permissions.clipboard,
+      cursor: ldkSettings.ldk.permissions.cursor,
+      filesystem: buildFilesystemPermissions(ldkSettings.ldk.permissions),
+      keyboard: ldkSettings.ldk.permissions.keyboard,
+      network: buildNetworkPermissions(ldkSettings.ldk.permissions),
+      process: ldkSettings.ldk.permissions.process,
+      ui: ldkSettings.ldk.permissions.ui,
+      vault: ldkSettings.ldk.permissions.vault,
+      whisper: ldkSettings.ldk.permissions.whisper,
+      window: ldkSettings.ldk.permissions.window,
     },
   });
   return Buffer.from(json).toString('base64');
--- ldk/javascript/src/webpack/ldk-settings.ts
+++ ldk/javascript/src/webpack/ldk-settings.ts
@@ -6,10 +6,42 @@ export interface Ldk {
     permissions: LdkPermissions
 }
 
+export interface LdkAptitude {
+    reason: string
+}
+
+export interface LdkValueReason {
+    value: string
+
+    reason: string
+}
+
+export interface LdkFilesystem {
+    pathGlobs:  LdkValueReason[]
+}
+
+export interface LdkNetwork {
+    urlDomains: LdkValueReason[]   
+}
+
 export interface LdkPermissions {
-    url: string[]
+    clipboard: LdkAptitude
 
-    filesystem: string[]
+    cursor: LdkAptitude
 
-    aptitude: string[]
+    filesystem: LdkFilesystem
+
+    keyboard: LdkAptitude
+
+    network: LdkNetwork
+
+    process: LdkAptitude
+
+    ui: LdkAptitude
+
+    vault: LdkAptitude
+
+    whisper: LdkAptitude
+
+    window: LdkAptitude
 }
