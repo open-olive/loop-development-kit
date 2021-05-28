ec3bc222d0646bad65b27840b7cff6fcfb18c975
Rich Seviora <richard.seviora@gmail.com>
2021-05-28 14:56:41 -0700

    [SIDE-2327] update menu

--- docs/src/components/menu/v2menu.tsx
+++ docs/src/components/menu/v2menu.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import { IMenuProps } from './shared-menu';
-import indexStyles from '../../pages/index.module.scss';
 import styles from './v2menu.module.scss';
 import OliveHelpsLogo from '../olive-helps-logo';
 
@@ -8,12 +6,20 @@ const DesktopV2Menu: React.FunctionComponent = (props) => (
   <div className={styles.desktopMenu}>
     <div className={styles.desktopMenuLogo}>
       <OliveHelpsLogo />
-    </div>
+    </div>z
     <div className={styles.desktopMenuTitle}>Developer Hub</div>
     <ul className={styles.desktopMenuLinks}>
-      <li className={styles.desktopMenuLink}>LDK</li>
-      <li className={styles.desktopMenuLink}>Design system</li>
-      <li className={styles.desktopMenuLink}>Support</li>
+      <li className={styles.desktopMenuLink}>
+        <a href="https://open-olive.github.io/loop-development-kit/ldk/javascript/" target="_blank">
+          LDK
+        </a>
+      </li>
+      <li className={styles.desktopMenuLink}>
+        <a href="https://coda.io/@olive-helps-design/design-system">Design system</a>
+      </li>
+      <li className={styles.desktopMenuLink}>
+        <a href="https://github.com/open-olive/loop-development-kit/issues">Support</a>
+      </li>
     </ul>
   </div>
 );
