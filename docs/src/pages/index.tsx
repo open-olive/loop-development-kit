e11727fadb1457e890137655d5b9dd8ed1c6c44e
Rich Seviora <richard.seviora@gmail.com>
2021-05-28 09:30:16 -0700

    [SIDE-2327] Update landing page contents, add icon assets

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -94,8 +94,11 @@ export default function Home(
         </div>
       </PageHeader>
       <Section sectionClassName={styles.sectionBackground}>
-        <h2 className={styles.sectionTitle}>Download</h2>
-        <p>Download the Olive Helps app.</p>
+        <h2 className={styles.sectionTitle}>Download Olive Helps</h2>
+        <p>
+          First things first: Download Olive Helps and create your Olive account. Then, visit the
+          Loop Library to become a Loop Author and get building.
+        </p>
         <div className={styles.downloadCollection}>
           <article className={styles.downloadItem}>
             <a href={downloadWindowsUrl} className={styles.downloadLink}>
@@ -104,74 +107,63 @@ export default function Home(
           </article>
           <article className={styles.downloadItem}>
             <a className={styles.downloadLink} href={downloadMacUrl}>
-              MacOS
+              Mac
             </a>
           </article>
         </div>
       </Section>
       <Section sectionClassName={styles.sectionBackground}>
-        <h2 className={styles.sectionTitle}>Get the LDK</h2>
-        <p>Get the LDK and start building!</p>
+        <h2 className={styles.sectionTitle}>Aptitude is everything</h2>
         <p>
-          <strong>PLEASE NOTE</strong>: In order to provide a highly controlled and secure
-          environment for our Loops, Olive Helps will be making a major version update to the LDK.
-          We will be transitioning to a JavaScript-only model and removing support for the C# and Go
-          LDKs. This will also enable us to build additional features and Aptitudes more quickly.
-          Please feel free to reach out to your Olive Helps developer contact if you have any
-          questions.
-        </p>
-        <div className={styles.downloadCollection}>
-          {languages.map((language) => (
-            <LanguageBlock {...language} key={language.language} />
-          ))}
-        </div>
-      </Section>
-      <Section sectionClassName={styles.sectionBackground}>
-        <h2 className={styles.sectionTitle}>See the Aptitudes</h2>
-        <p>
-          Olive Helps offers different aptitudes that enable you to build Loops that help users do
-          their job
+          As you build Loops, leverage Olive’s collection of Aptitudes — tools and capabilities that
+          capture important user information. Aptitudes enable Sensors, which let Olive understand
+          how users are working and when to offer help.
         </p>
         <div className={styles.aptitudeList}>{aptitudeItems}</div>
       </Section>
       <Section sectionClassName={styles.sectionBackground}>
-        <h2 className={styles.sectionTitle}>Read the Guides</h2>
+        <h2 className={styles.sectionTitle}>Design system</h2>
+        <p>Create beautiful Loops that seamlessly integrated with Olive Helps.</p>
+        <a href="">View the design system</a>
+      </Section>
+      <Section sectionClassName={styles.sectionBackground}>
+        <h2 className={styles.sectionTitle}>Additional Resources</h2>
         <p>We have guides available to help you at various steps.</p>
         <div className={styles.aptitudeList}>{guideItems}</div>
-      </Section>
-      <Section sectionClassName={styles.sectionBackground}>
-        <h2 className={styles.sectionTitle}>Getting Help</h2>
-        <p>
-          Our Olive Helps developers are here to help you succeed! Visit{' '}
-          <a
-            href="https://github.com/open-olive/loop-development-kit/issues"
-            target="_blank"
-            rel="noopener"
-          >
-            this page
-          </a>{' '}
-          to submit an issue or request, or email your Olive Helps developer contact if you need any
-          additional help.
-        </p>
+        <div>
+          <h3>Need help getting started?</h3>
+          <p>
+            Submit a request or email your Olive Helps developer contact for further assistance.
+          </p>
+          <a href="">Submit a request</a>
+        </div>
       </Section>
       <Section sectionClassName={styles.sectionBackground}>
         <p>
-          <a
-            className={styles.licenseLink}
-            href="https://github.com/open-olive/loop-development-kit/blob/main/LICENSE"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Loop Development Kit Open Source License
-          </a>
-          {'|'}
           <a
             className={styles.termsLink}
             href="https://olive.page.link/olive-helps-terms"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Olive Helps Terms &amp; Conditions
+            About Olive
+          </a>
+
+          <a
+            className={styles.termsLink}
+            href="https://olive.page.link/olive-helps-terms"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Terms and Conditions
+          </a>
+          <a
+            className={styles.licenseLink}
+            href="https://github.com/open-olive/loop-development-kit/blob/main/LICENSE"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LDK Open Source License
           </a>
         </p>
       </Section>
