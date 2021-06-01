231ebc0ae7fe8d3c4e9c90d3f0c16c9171bc307b
Cory Shields <cory.shields@oliveai.com>
2021-06-01 17:15:16 -0400

    SIDE-2143: Some more code review suggestions

--- ldk/javascript/src/whisper/form.ts
+++ ldk/javascript/src/whisper/form.ts
@@ -1,20 +1,20 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-explicit-any */ // We need to dynamically map child components to determine if they are onChange or onSelect flavored inputs.
 import { Components, Form, Whisper, WhisperComponentType } from '.';
 
 export class LdkForm {
     children: Array<Components>;
 
-    componentState: Map<string, any>;
+    componentState: Map<string, string | number | boolean>;
 
     constructor(children: Array<Components>) {
-        this.componentState = new Map<string, any>();
+        this.componentState = new Map<string, string |number | boolean>();
         this.children = children;
 
         this.setInitialComponentState(this.children);
         this.registerListeners(this.children);
     }
 
-    getComponentState(): Map<string, any> {
+    getComponentState(): Map<string, string | number | boolean> {
         return this.componentState;
     }
 
@@ -27,27 +27,27 @@ export class LdkForm {
     }
 
     private setInitialComponentState(children: Array<any>) {
-        children.forEach((child: any) => {
+        children?.forEach((child: any) => {
             if (this.isOnChangeInput(child)) {
-                this.componentState = this.componentState.set(child.name, child.value);
+                this.componentState.set(child.name, child.value);
             } else if (this.isOnSelectInput(child)) {
-                this.componentState = this.componentState.set(child.name, child.selected);
+                this.componentState.set(child.name, child.selected);
             }
         });
     }
 
     private registerListeners(children: Array<any>) {
-        children.forEach((child: any) => {
+        children?.forEach((child: any) => {
             if (this.isOnChangeInput(child)) {
                 const incomingOnChange = child.onChange;
                 child.onChange = (error: Error | undefined, param: string, whisper: Whisper) => { // eslint-disable-line no-param-reassign
-                    this.componentState = this.componentState.set(child.name, param);
+                    this.componentState.set(child.name, param);
                     incomingOnChange(error, param, whisper);
                 }
             } else if (this.isOnSelectInput(child)) {
                 const incomingOnSelect = child.onSelect;
                 child.onSelect = (error: Error | undefined, param: string, whisper: Whisper) => { // eslint-disable-line no-param-reassign
-                    this.componentState = this.componentState.set(child.name, param);
+                    this.componentState.set(child.name, param);
                     incomingOnSelect(error, param, whisper);
                 }
             }
