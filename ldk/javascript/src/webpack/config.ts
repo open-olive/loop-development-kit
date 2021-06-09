565d4a7386b475735e966f294f248e800fd68125
Rich Seviora <richard.seviora@gmail.com>
2021-06-09 15:22:57 -0700

    [SIDE-2331] Document eslint rule disable

--- ldk/javascript/src/webpack/config.ts
+++ ldk/javascript/src/webpack/config.ts
@@ -4,7 +4,8 @@ import Terser from 'terser-webpack-plugin';
 import { generateBanner } from './generate-banner';
 import { LdkSettings } from './ldk-settings'; // Need to dynamically refer to Loop's package.json
 
-/* eslint-disable-next-line */
+// Suppressing rule as we intentionally want a dynamic require.
+// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-dynamic-require
 const ldkSettings: LdkSettings = require(path.join(process.cwd(), '/package.json'));
 
 const config: webpack.Configuration = {
