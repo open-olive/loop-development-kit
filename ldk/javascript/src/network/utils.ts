a2871b22ec89b07db42df1ee28ec63329c28c3c4
Ruslan Guryanov <ruslan.guryanov@oliveai.com>
2021-07-27 13:59:04 -0400

    HELPS-963: RG - adding check for the decoding array size before the bom trim

--- ldk/javascript/src/network/utils.ts
+++ ldk/javascript/src/network/utils.ts
@@ -1,5 +1,5 @@
 export const stripBom = (value: Uint8Array): Uint8Array => {
-  if (value[0] === 0xef && value[1] === 0xbb && value[2] === 0xbf) {
+  if (value.length > 2 && value[0] === 0xef && value[1] === 0xbb && value[2] === 0xbf) {
     return value.slice(3);
   }
   return value;
