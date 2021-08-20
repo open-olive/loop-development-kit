cfdaf46baaad41ac8e746d5ec60810f768349574
Maggie Ma <maggie.ma@oliveai.com>
2021-08-20 11:37:25 -0400

    Updated selfTest w/ Eric

--- ldk/javascript/examples/self-test-loop/src/config/excel-group.ts
+++ ldk/javascript/examples/self-test-loop/src/config/excel-group.ts
@@ -3,18 +3,18 @@ import { LoopTest } from '../testingFixtures/loopTest';
 
 import * as excelTests from '../tests/excel';
 
-export const excelTestGroup = (): TestGroup=>
-new TestGroup('Excel Aptitude',[
+export const excelTestGroup = (): TestGroup =>
+  new TestGroup('Excel Aptitude', [
     new LoopTest(
-        'Excel Aptitude - Encode test',
-        excelTests.testExcelEncode,
-        5000,
-        'Encodes a workbook object into Uint8Array data. Should succeed.',
+      'Excel Aptitude - Encode test',
+      excelTests.testExcelEncode,
+      5000,
+      'Encodes a workbook object into Uint8Array data. Should succeed.',
     ),
-    new LoopTest(
-        'Excel Aptitude - Decode test',
-        excelTests.testExcelDecode,
-        5000,
-        'Decodes Uint8Array data into a workbook object. Should succeed.',
-    ),
-]);
+    // new LoopTest(
+    //     'Excel Aptitude - Decode test',
+    //     excelTests.testExcelDecode,
+    //     5000,
+    //     'Decodes Uint8Array data into a workbook object. Should succeed.',
+    // ),
+  ]);
