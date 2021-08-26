c93225b8338ce58ae7f1e3dd2ecaec2a2f4232e0
Daniel Wagers (Olive) <77338546+DanielWagersOlive@users.noreply.github.com>
2021-08-26 13:15:35 -0400

    initial reconnect code

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,100 +1,94 @@
 chrome.runtime.onInstalled.addListener(() => {
-  const ws = new WebSocket("ws://localhost:24984")
-  ws.addEventListener('open', function(event) {
-    console.log('Connection open')
-  });
+  let ws;
+  let connect = () => {
+    if (ws) {
+      ws.close();
+    } else {
+      console.log('connecting');
+      ws = new WebSocket("ws://localhost:24984");
 
-  ws.addEventListener('message', function (event) {
-    let callMessage = {};
-    try {
-      callMessage = JSON.parse(event.data);
-    } catch (e) {
-      console.log("error parsing call message: " + e);
-    }
+      ws.addEventListener('close', (event) => {
+        setTimeout(connect, 1000);
+      });
 
-    switch (callMessage.type) {
-      case 'OpenTabCall':
-        chrome.tabs.create({
-          url: callMessage.args.address
-        }).then((tab) => {
-          ws.send(JSON.stringify({
-            'type': 'OpenTabReturn',
-            'version': 0,
-            'callId': callMessage.callId,
-            'return': {
-              'tabId': tab.id,
-              'err': ''
-            }
-          }));
-        }, (reason) => {
-          ws.send(JSON.stringify({
-            'type': 'OpenTabReturn',
-            'version': 0,
-            'callId': callMessage.callId,
-            'return': {
-              'tabId': -1,
-              'err': 'promise rejected: ' + reason
-            }
-          }));
-        });
-      break;
+      ws.addEventListener('open', (event) => {
+        console.log('connected');
 
-      case 'OpenWindowCall':
-        chrome.windows.create({
-          url: callMessage.args.address
-        }).then((window) => {
-          ws.send(JSON.stringify({
-            'type': 'OpenWindowReturn',
-            'version': 0,
-            'callId': callMessage.callId,
-            'return': {
-              'windowId': window.id,
-              'err': ''
-            }
-          }));
-        }, (reason) => {
-          ws.send(JSON.stringify({
-            'type': 'OpenWindowReturn',
-            'version': 0,
-            'callId': callMessage.callId,
-            'return': {
-              'windowId': -1,
-              'err': 'promise rejected: ' + reason
-            }
-          }));
-        });
-      break;
-
-      default:
-        console.log('unknown call message type');
-      break;
-    }
-  });
-
-  chrome.webNavigation.onBeforeNavigate.addListener((data) => {
-    ws.send(JSON.stringify({
-      'type': 'NavigationEvent',
-      'version': 0,
-      'event': data
-    }));
-  });
-
-  chrome.extension.onRequest.addListener((request, sender, sendResponse) => {
-    switch(request.message) {
-      case 'TextSelection':
-        ws.send(JSON.stringify({
-          'type': 'TextSelectionEvent',
-          'version': 0,
-          'event': {
-            'selectedText': request.data
+        ws.addEventListener('message', function (event) {
+          let callMessage = {};
+          try {
+            callMessage = JSON.parse(event.data);
+          } catch (e) {
+            console.log("error parsing call message: " + e);
           }
-        }));
-      break;
 
-      default:
-        sendResponse({data: 'invalid arguments'});
-      break;
+          switch (callMessage.type) {
+            case 'OpenTabCall':
+              chrome.tabs.create({
+                url: callMessage.args.address
+              }, (tab) => {
+                ws.send(JSON.stringify({
+                  'type': 'OpenTabReturn',
+                  'version': 0,
+                  'callId': callMessage.callId,
+                  'return': {
+                    'tabId': tab.id,
+                    'err': ''
+                  }
+                }));
+              });
+            break;
+
+            case 'OpenWindowCall':
+              chrome.windows.create({
+                url: callMessage.args.address
+              }, (window) => {
+                ws.send(JSON.stringify({
+                  'type': 'OpenWindowReturn',
+                  'version': 0,
+                  'callId': callMessage.callId,
+                  'return': {
+                    'windowId': window.id,
+                    'err': ''
+                  }
+                }));
+              });
+              break;
+
+            default:
+              console.log('unknown call message type');
+            break;
+          }
+        });
+
+        chrome.webNavigation.onBeforeNavigate.addListener((data) => {
+          ws.send(JSON.stringify({
+            'type': 'NavigationEvent',
+            'version': 0,
+            'event': data
+          }));
+        });
+
+        chrome.extension.onRequest.addListener((request, sender, sendResponse) => {
+          switch(request.message) {
+            case 'TextSelection':
+              ws.send(JSON.stringify({
+                'type': 'TextSelectionEvent',
+                'version': 0,
+                'event': {
+                  'selectedText': request.data
+                }
+              }));
+            break;
+
+            default:
+              sendResponse({data: 'invalid arguments'});
+            break;
+          }
+        });
+      });
     }
-  });
-});
+  };
 
+  connect();
+});
