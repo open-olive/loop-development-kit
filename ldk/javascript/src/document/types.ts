f6889f0efb0c1663807a072d0f385e42ebeb755c
Ruslan Guryanov <ruslan.guryanov@oliveai.com>
2021-09-01 14:30:23 -0400

    HELPS-1148: running prettier

--- ldk/javascript/src/document/types.ts
+++ ldk/javascript/src/document/types.ts
@@ -1,21 +1,19 @@
-
 export interface Workbook {
-    worksheets: Worksheet[];
-  }
-  
-  export interface Worksheet {
-    hidden: boolean;
-    hiddenColumns: number[];
-    hiddenRows: number[];
-    name: string;
-    rows: Row[];
-  }
-  
-  export interface Row {
-    cells: Cell[];
-  }
-  
-  export interface Cell {
-    value: string;
-  }
-  
+  worksheets: Worksheet[];
+}
+
+export interface Worksheet {
+  hidden: boolean;
+  hiddenColumns: number[];
+  hiddenRows: number[];
+  name: string;
+  rows: Row[];
+}
+
+export interface Row {
+  cells: Cell[];
+}
+
+export interface Cell {
+  value: string;
+}
