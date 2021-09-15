1824fc24a0554835fa1e24bed9ded1cad40eeff8
Maggie Ma <maggie.ma@oliveai.com>
2021-09-15 15:47:25 -0400

    Updated text

--- ldk/javascript/examples/self-test-loop/src/tests/whisper/index.ts
+++ ldk/javascript/examples/self-test-loop/src/tests/whisper/index.ts
@@ -22,6 +22,7 @@ import {
 } from '@oliveai/ldk/dist/whisper/types';
 import { stripIndent } from 'common-tags';
 import { logMap, resolveRejectButtons } from './utils';
+import {shortText, longText, markdownText} from'./text';
 
 export const testIconLayout = (): Promise<boolean> =>
   new Promise(async (resolve, reject) => {
@@ -114,23 +115,6 @@ export const testIconLayout = (): Promise<boolean> =>
 export const testMarkdownWhisper = (): Promise<boolean> =>
   new Promise(async (resolve, reject) => {
     const options = ['M12.01', 'M00.123'];
-    const markdown = stripIndent`
-      A paragraph with *emphasis* and **strong importance**.
-      # H1 Markdown Example 
-      ## H2 Markdown Example 
-      ### H3 Markdown Example 
-      > A block quote with ~strikethrough~ and a URL: https://oliveai.com/
-
-      * Lists
-      * [ ] todo
-      * [x] done
-
-      A table:
-
-      | Table Header 1 | Table header 2 |
-      | - | - |
-      | Row 1 Col 1 | Row 1 Col 2 |
-      | Row 2 Col 1 | Row 2 Col 2 |`;
 
     await whisper.create({
       label: 'Markdown whisper Test',
@@ -139,7 +123,7 @@ export const testMarkdownWhisper = (): Promise<boolean> =>
       },
       components: [
         {
-          body: markdown,
+          body: markdownText,
           type: WhisperComponentType.Markdown,
         },
         {
@@ -2353,23 +2337,7 @@ export const testScrollInsideBox = (): Promise<boolean> =>
               },
               {
                 type: whisper.WhisperComponentType.Markdown,
-                body: stripIndent`
-              A paragraph with *emphasis* and **strong importance**.
-              this is the test of scroll feature
-              
-              
-              
-            
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut molestie feugiat est, eget mollis metus sollicitudin tincidunt. Integer ut lorem in nisl gravida placerat sed eget eros. Fusce finibus velit quam, in tincidunt dui consectetur rutrum. Donec venenatis sem in nulla malesuada, a lobortis nisi volutpat. Vivamus vitae posuere quam. Praesent eget vestibulum risus. Fusce posuere diam elit, eu aliquet nunc laoreet nec. Vestibulum viverra turpis ac ligula euismod, eget mollis ligula faucibus. Nam dictum viverra sem in imperdiet. Donec tempus ullamcorper rhoncus.
-
-Donec efficitur diam nec nisl congue euismod. Integer sit amet bibendum ante. Duis non eleifend enim. Vestibulum ligula urna, lobortis id efficitur sed, tincidunt in magna. Nam velit elit, congue ac condimentum nec, pharetra eget mi. Nunc fringilla odio eget ipsum blandit rutrum. Fusce maximus dapibus urna, sodales porttitor turpis tempus in. Curabitur gravida congue nibh tincidunt interdum.
-
-Fusce iaculis euismod tellus, quis tincidunt nulla ultricies sed. Proin lacinia eros lorem, id luctus erat pulvinar eu. Donec gravida sollicitudin consequat. Cras sed erat a ante fringilla accumsan a in tellus. Fusce porta, felis id fringilla pellentesque, nisi orci hendrerit urna, vel dictum sapien lacus et purus. Nulla auctor metus sit amet ante fermentum laoreet. Fusce iaculis porta urna, sed euismod nulla gravida in.
-
-Pellentesque sodales enim eget elit dapibus auctor. Nulla facilisi. Ut auctor interdum ex sed vestibulum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec ornare, justo sed blandit viverra, odio massa tristique dui, in gravida ante nisl at arcu. Donec fermentum libero nec quam hendrerit, et accumsan velit placerat. Sed est urna, finibus commodo nibh et, posuere blandit dolor. Duis luctus augue ante, congue luctus odio molestie vel. Phasellus quis porttitor turpis. Vivamus quis est et massa placerat molestie ut at arcu. Mauris ornare velit velit, in lobortis nibh venenatis eu.
-
-Morbi ac ligula pellentesque, molestie quam id, pellentesque augue. Nunc condimentum mollis erat, ac tempor nulla consequat nec. Nunc turpis velit, blandit id venenatis id, tristique eu lacus. Duis et neque non purus placerat vestibulum. Morbi aliquet enim ut lacus sollicitudin, ut gravida augue sodales. Sed tristique ipsum efficitur dui cursus eleifend. Nunc in arcu sollicitudin, egestas quam ac, tincidunt sapien. Nam ac nunc sed lorem maximus suscipit. Donec sollicitudin magna nec sollicitudin bibendum. Phasellus sed ex porta, egestas purus vitae, tincidunt dolor. Praesent efficitur et mauris sit amet placerat.
-`,
+                body: shortText,
               },
             ],
           },
@@ -2401,70 +2369,7 @@ Morbi ac ligula pellentesque, molestie quam id, pellentesque augue. Nunc condime
             children: [
               {
                 type: whisper.WhisperComponentType.Markdown,
-                body: stripIndent`
-              
-              This is the test of scroll feature.
-              When the customHeight is not enough, it will scroll.
-              
-              
-              
-            
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-               Ut molestie feugiat est, eget mollis metus sollicitudin 
-               tincidunt. Integer ut lorem in nisl gravida placerat sed 
-               eget eros. Fusce finibus velit quam, in tincidunt dui 
-               consectetur rutrum. Donec venenatis sem in nulla malesuada,
-                a lobortis nisi volutpat. Vivamus vitae posuere quam. 
-                Praesent eget vestibulum risus. Fusce posuere diam elit, 
-                eu aliquet nunc laoreet nec. Vestibulum viverra turpis 
-                ac ligula euismod, eget mollis ligula faucibus. Nam dictum 
-                viverra sem in imperdiet. Donec tempus ullamcorper rhoncus.
-
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ut molestie feugiat est, eget mollis metus sollicitudin 
-                tincidunt. Integer ut lorem in nisl gravida placerat sed 
-                eget eros. Fusce finibus velit quam, in tincidunt dui 
-                consectetur rutrum. Donec venenatis sem in nulla malesuada,
-                 a lobortis nisi volutpat. Vivamus vitae posuere quam. 
-                 Praesent eget vestibulum risus. Fusce posuere diam elit, 
-                 eu aliquet nunc laoreet nec. Vestibulum viverra turpis 
-                 ac ligula euismod, eget mollis ligula faucibus. Nam dictum 
-                 viverra sem in imperdiet. Donec tempus ullamcorper rhoncus.
-
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                 Ut molestie feugiat est, eget mollis metus sollicitudin 
-                 tincidunt. Integer ut lorem in nisl gravida placerat sed 
-                 eget eros. Fusce finibus velit quam, in tincidunt dui 
-                 consectetur rutrum. Donec venenatis sem in nulla malesuada,
-                  a lobortis nisi volutpat. Vivamus vitae posuere quam. 
-                  Praesent eget vestibulum risus. Fusce posuere diam elit, 
-                  eu aliquet nunc laoreet nec. Vestibulum viverra turpis 
-                  ac ligula euismod, eget mollis ligula faucibus. Nam dictum 
-                  viverra sem in imperdiet. Donec tempus ullamcorper rhoncus.
-
-
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                 Ut molestie feugiat est, eget mollis metus sollicitudin 
-                 tincidunt. Integer ut lorem in nisl gravida placerat sed 
-                 eget eros. Fusce finibus velit quam, in tincidunt dui 
-                 consectetur rutrum. Donec venenatis sem in nulla malesuada,
-                  a lobortis nisi volutpat. Vivamus vitae posuere quam. 
-                  Praesent eget vestibulum risus. Fusce posuere diam elit, 
-                  eu aliquet nunc laoreet nec. Vestibulum viverra turpis 
-                  ac ligula euismod, eget mollis ligula faucibus. Nam dictum 
-                  viverra sem in imperdiet. Donec tempus ullamcorper rhoncus.
-
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                 Ut molestie feugiat est, eget mollis metus sollicitudin 
-                 tincidunt. Integer ut lorem in nisl gravida placerat sed 
-                 eget eros. Fusce finibus velit quam, in tincidunt dui 
-                 consectetur rutrum. Donec venenatis sem in nulla malesuada,
-                  a lobortis nisi volutpat. Vivamus vitae posuere quam. 
-                  Praesent eget vestibulum risus. Fusce posuere diam elit, 
-                  eu aliquet nunc laoreet nec. Vestibulum viverra turpis 
-                  ac ligula euismod, eget mollis ligula faucibus. Nam dictum 
-                  viverra sem in imperdiet. Donec tempus ullamcorper rhoncus.
-`,
+                body: longText,
               },
             ],
           },
