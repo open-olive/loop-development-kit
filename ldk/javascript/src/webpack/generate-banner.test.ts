1545855622a6cb8be67c4af36bea3cdf59375a36
Rich Seviora <richard.seviora@gmail.com>
2021-09-23 11:14:33 -0700

    [HELPS-1579] update test with correct contract version

--- ldk/javascript/src/webpack/generate-banner.test.ts
+++ ldk/javascript/src/webpack/generate-banner.test.ts
@@ -42,7 +42,7 @@ describe('Generate Banner', () => {
   it('generates banner given valid LdkSettings', () => {
     const actual = getLoopMetadataContent(generateBanner(ldkSettings));
     const expected = {
-      oliveHelpsContractVersion: '0.1.4',
+      oliveHelpsContractVersion: '0.1.5',
       permissions: {
         clipboard: {},
         cursor: {},
@@ -79,7 +79,7 @@ describe('Generate Banner', () => {
     const actual = getLoopMetadataContent(generateBanner(ldkSettings));
 
     const expected = {
-      oliveHelpsContractVersion: '0.1.4',
+      oliveHelpsContractVersion: '0.1.5',
       permissions: {
         filesystem: { pathGlobs: [{ value: '/my/path' }] },
         network: { urlDomains: [{ value: '*.google.com' }] },
