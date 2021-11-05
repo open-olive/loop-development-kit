2be2ecc8fe2a493c741f7e8c479e8516566f6711
Maggie Ma <maggie.ma@oliveai.com>
2021-11-05 10:49:10 -0400

    added ignore

--- ldk/javascript/src/screen/index.ts
+++ ldk/javascript/src/screen/index.ts
@@ -4,10 +4,11 @@ import { OCRResult, OCRCoordinates } from './types';
 export * from './types';
 
 /**
- *  Screen aptitude allows Loops to enable Optical character recognition ability.
+ *  @ignore Screen aptitude allows Loops to enable Optical character recognition ability.
  */
 export interface Screen {
   /**
+   * @ignore
    * Performs screen OCR and returns recognized text ressults.
    * @param  ocrCoordinates? - The term that will be used to identify the coordination of cursor???
    * @returns A Promise resolving with the text results as a string.
