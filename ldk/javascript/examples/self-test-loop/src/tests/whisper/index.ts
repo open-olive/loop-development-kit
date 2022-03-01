5f23f5b7e6e5a45b1ff98836721dfc4375c51332
Josef Budniakiewicz <josef.budniakiewicz@oliveai.com>
2022-03-01 14:03:58 -0500

    [HELPS-2984] Adding Pagination self tests

--- ldk/javascript/examples/self-test-loop/src/tests/whisper/index.ts
+++ ldk/javascript/examples/self-test-loop/src/tests/whisper/index.ts
@@ -28,19 +28,18 @@ import {
 import { stripIndent } from 'common-tags';
 import {
   autocompleteOptions,
-  ignoreCase,
   createAutocompleteComponent,
   createDivider,
   createTextComponent,
   createSelectComponent,
   logMap,
-  matchFrom,
   onActionWrapper,
   resolveRejectButtons,
 } from './utils';
-import { shortText, longText, markdownText, image } from './text';
+import { longText, markdownText, image } from './text';
 
 export * from './autocomplete';
+export * from './pagination';
 
 export const testIconLayout = (): Promise<boolean> =>
   new Promise(async (resolve, reject) => {
@@ -3456,15 +3455,15 @@ export const testDisabledInputs = (): Promise<boolean> =>
   });
 
 export const testDropZoneOnRemove = async (): Promise<boolean> => {
-  const resultArray = new Array();
-  const deletedFile = new Array();
+  const resultArray: string[] = [];
+  const deletedFile: string[] = [];
   const mapFile = new Map();
 
   const dropZone: whisper.DropZone = {
     type: WhisperComponentType.DropZone,
     onDrop: (error, param) => {
       if (param) {
-        param.map((file) => {
+        param.forEach((file) => {
           mapFile.set(file.path, `Path: ${file.path}, Size: ${file.size}`);
         });
         console.log('onDrop called: ');
@@ -3485,7 +3484,7 @@ export const testDropZoneOnRemove = async (): Promise<boolean> => {
       });
       const remainingFiles = await onRemoveAction;
       // Get remaining files and delete them from mapFIle to get deleted files.
-      remainingFiles.map((file) => {
+      remainingFiles.forEach((file) => {
         console.log('onRemove called : files are: ', file.path);
         if (mapFile.has(file.path)) {
           mapFile.delete(file.path);
@@ -3500,11 +3499,11 @@ export const testDropZoneOnRemove = async (): Promise<boolean> => {
 
       // Add remaining files to mapFile
       mapFile.clear();
-      remainingFiles.map((remainingFile) => {
+      remainingFiles.forEach((remainingFile) => {
         mapFile.set(remainingFile.path, `Path: ${remainingFile.path}, Size: ${remainingFile.size}`);
       });
       resultArray.splice(0, resultArray.length);
-      mapFile.forEach((fileValue, file) => {
+      mapFile.forEach((fileValue) => {
         console.log('fileValue: ', fileValue);
         resultArray.push(`${fileValue} \n\n `);
       });
@@ -3524,7 +3523,7 @@ export const testDropZoneOnRemove = async (): Promise<boolean> => {
     ],
   });
 
-  await mapFile.forEach((fileValue, file) => {
+  await mapFile.forEach((fileValue) => {
     // console.log('fileValue: ', fileValue);
     resultArray.push(`${fileValue} \n\n `);
   });
