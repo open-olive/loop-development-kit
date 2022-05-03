5a41aa678efe4bdcf862e5849c09096548381b7e
Maggie Ma <maggie.ma@oliveai.com>
2022-05-03 16:41:44 -0400

    update according to comments

--- ldk/javascript/src/webpack/CheckWarningsAndErrorsPlugin.ts
+++ ldk/javascript/src/webpack/CheckWarningsAndErrorsPlugin.ts
@@ -1,9 +1,7 @@
 import { Compiler, WebpackError } from 'webpack';
 
 const assetName = 'loop.js';
-const warningMsg =
-  'Please provide a way for users to open your Loop. You can do this using the ui.loopOpenHandler. This is required to submit your Loop.';
-const plugin = 'CheckLoopOpenHandlerPlugin';
+const plugin = 'CheckWarningsAndErrorsPlugin';
 export class CheckWarningsAndErrorsPlugin {
   apply(compiler: Compiler): void {
     compiler.hooks.thisCompilation.tap(plugin, (compilation) => {
@@ -12,6 +10,9 @@ export class CheckWarningsAndErrorsPlugin {
         if (loopjs) {
           const fileContent = loopjs.source();
           if (!fileContent?.includes('ui.loopOpenHandler(')) {
+            const warningMsg =
+              'Please provide a way for users to open your Loop. You can do this using the ui.loopOpenHandler. This is required to submit your Loop.';
+
             const openHandlerMissingWarning = new WebpackError(warningMsg);
             openHandlerMissingWarning.file = assetName;
             compilation.warnings.push(openHandlerMissingWarning);
@@ -19,8 +20,9 @@ export class CheckWarningsAndErrorsPlugin {
           if (fileContent?.includes('screen.ocr(')) {
             const ocrDeprecationWarningMsg =
               '[screen.ocr] ocr will be deprecated soon. Please use listenOcrMonitor instead.';
-            const openHandlerMissingWarning = new WebpackError(ocrDeprecationWarningMsg);
-            compilation.warnings.push(openHandlerMissingWarning);
+            const ocrDeprecationWarning = new WebpackError(ocrDeprecationWarningMsg);
+            ocrDeprecationWarning.file = assetName;
+            compilation.warnings.push(ocrDeprecationWarning);
           }
         }
       });
