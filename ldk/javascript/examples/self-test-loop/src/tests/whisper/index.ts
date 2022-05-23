3486c2e543e15150c5825ad0c6fda24efd44e3e5
Jeff Sun <jeff.sun@oliveai.com>
2022-05-23 09:51:01 -0400

    update text for test

--- ldk/javascript/examples/self-test-loop/src/tests/whisper/index.ts
+++ ldk/javascript/examples/self-test-loop/src/tests/whisper/index.ts
@@ -489,7 +489,7 @@ export const testBoxInBoxWithDynamicHeight = (): Promise<boolean> =>
                     type: WhisperComponentType.Markdown,
                     body: stripIndent`
                     **Header Left with Min Height - Large**
-                    Some text on the right
+                    Some text on the Left
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                     when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
