3a00f4e94e8a18b9221f55cd3b58ac8373c18b26
harrisonkunkel <103070329+harrisonkunkel@users.noreply.github.com>
2022-06-13 10:37:43 -0400

    HELPS-4694-listen2-function (#597)
    * listen2 function
    
    * removed listen and includeOliveHelpsEvents
    
    * ran prettier
    
    * Updated Internal Version to 3.18.0
    
    * changed listen2 to listen all and filter in LDK
    
    * fixed linting
    
    * added window aptitude mock for jest tests
    
    * rans prettier
    
    * fixed lint error
    
    * clean up
    
    * removed console logs
    
    * ren prettier
    
    * fixed issue with seting active window
    
    * clean up
    
    * ran prettier
    
    * clean up focusWindow const
    
    * added listen back and change new listen function to listenWithOptions
    
    * ran prettier
    
    * fixed syntax
    
    Co-authored-by: Hector Rivera <hector.rivera@oliveai.com>
    Co-authored-by: hector-rivera-olive <93994266+hector-rivera-olive@users.noreply.github.com>

--- src/clipboard/clipboard.test.ts
+++ src/clipboard/clipboard.test.ts
@@ -5,34 +5,18 @@ describe('Clipboard', () => {
   beforeEach(() => {
     oliveHelps.clipboard = {
       includeOliveHelpsEvents: jest.fn(),
+      listen: jest.fn(),
+      listenAll: jest.fn(),
       read: jest.fn(),
       write: jest.fn(),
-      listen: jest.fn(),
     };
-  });
-
-  describe('read', () => {
-    it('returns a promise result with expected clipboard value', () => {
-      const expected = 'expected string';
-      mocked(oliveHelps.clipboard.read).mockImplementation((callback) =>
-        callback(undefined, expected),
-      );
-
-      const actual = clipboard.read();
-
-      return expect(actual).resolves.toBe(expected);
-    });
-
-    it('returns a rejected promise', () => {
-      const exception = 'Exception';
-      mocked(oliveHelps.clipboard.read).mockImplementation(() => {
-        throw exception;
-      });
-
-      const actual = clipboard.read();
-
-      return expect(actual).rejects.toBe(exception);
-    });
+    oliveHelps.window = {
+      activeWindow: jest.fn(),
+      listenActiveWindow: jest.fn(),
+      all: jest.fn(),
+      listenAll: jest.fn(),
+      getActiveWindowID: jest.fn(),
+    };
   });
 
   describe('listen', () => {
@@ -72,6 +56,156 @@ describe('Clipboard', () => {
 
       const callback = jest.fn();
       expect(() => clipboard.listen(false, callback)).rejects.toBe(exception);
+    });
+  });
+
+  describe('read', () => {
+    it('returns a promise result with expected clipboard value', () => {
+      const expected = 'expected string';
+      mocked(oliveHelps.clipboard.read).mockImplementation((callback) =>
+        callback(undefined, expected),
+      );
+
+      const actual = clipboard.read();
+
+      return expect(actual).resolves.toBe(expected);
+    });
+
+    it('returns a rejected promise', () => {
+      const exception = 'Exception';
+      mocked(oliveHelps.clipboard.read).mockImplementation(() => {
+        throw exception;
+      });
+
+      const actual = clipboard.read();
+
+      return expect(actual).rejects.toBe(exception);
+    });
+  });
+
+  describe('listenWithOptions', () => {
+    it('sets clipboard olive helps configuration', () => {
+      const callback = jest.fn();
+      clipboard.listenWithOptions({ includeOliveHelpsEvents: true }, callback);
+
+      expect(oliveHelps.clipboard.listenAll).toHaveBeenCalledWith(
+        expect.any(Function),
+        expect.any(Function),
+      );
+    });
+
+    it('passed in listen Including olive helps events function to Olive Helps', async () => {
+      const callback = jest.fn();
+      const text = 'abc';
+      const expectedWindowInfo = {
+        title: 'Olive Helps Application',
+        path: 'path/to/my/window',
+        pid: 8675309,
+        x: 0,
+        y: 1,
+        width: 9001,
+        height: 50,
+      };
+      mocked(oliveHelps.window.activeWindow).mockImplementation((callbackAW) =>
+        callbackAW(undefined, expectedWindowInfo),
+      );
+      mocked(oliveHelps.clipboard.listenAll).mockImplementation((listenerCb, returnCb) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        returnCb({} as any);
+        listenerCb(undefined, text);
+      });
+
+      await clipboard.listenWithOptions({ includeOliveHelpsEvents: true }, callback);
+      expect(callback).toHaveBeenCalledWith(text);
+    });
+
+    it('passed in listen Including olive helps events function to Chrome', async () => {
+      const callback = jest.fn();
+      const expectedWindowInfo = {
+        title: 'Chrome',
+        path: 'path/to/my/window',
+        pid: 8675309,
+        x: 0,
+        y: 1,
+        width: 9001,
+        height: 50,
+      };
+      const text = 'abc';
+
+      mocked(oliveHelps.window.activeWindow).mockImplementation((callbackAW) =>
+        callbackAW(undefined, expectedWindowInfo),
+      );
+      mocked(oliveHelps.clipboard.listenAll).mockImplementation((listenerCb, returnCb) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        returnCb({} as any);
+        listenerCb(undefined, text);
+      });
+
+      await clipboard.listenWithOptions({ includeOliveHelpsEvents: true }, callback);
+      expect(callback).toHaveBeenCalledWith(text);
+    });
+    it('passed in listen excluding olive helps events function to Olive Helps', async () => {
+      const callback = jest.fn();
+      const expectedWindowInfo = {
+        title: 'Olive Helps Application',
+        path: 'path/to/my/window',
+        pid: 8675309,
+        x: 0,
+        y: 1,
+        width: 9001,
+        height: 50,
+      };
+      const text = 'abc';
+
+      mocked(oliveHelps.window.activeWindow).mockImplementation((callbackAW) =>
+        callbackAW(undefined, expectedWindowInfo),
+      );
+      mocked(oliveHelps.clipboard.listenAll).mockImplementation((listenerCb, returnCb) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        returnCb({} as any);
+        listenerCb(undefined, text);
+      });
+
+      await clipboard.listenWithOptions({ includeOliveHelpsEvents: false }, callback);
+      expect(callback).not.toHaveBeenCalled();
+    });
+
+    it('passed in listen excluding olive helps events function to Chrome', async () => {
+      const callback = jest.fn();
+      const expectedWindowInfo = {
+        title: 'Chrome',
+        path: 'path/to/my/window',
+        pid: 8675309,
+        x: 0,
+        y: 1,
+        width: 9001,
+        height: 50,
+      };
+      const text = 'abc';
+
+      mocked(oliveHelps.window.activeWindow).mockImplementation((callbackAW) =>
+        callbackAW(undefined, expectedWindowInfo),
+      );
+      mocked(oliveHelps.clipboard.listenAll).mockImplementation((listenerCb, returnCb) => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        returnCb({} as any);
+        listenerCb(undefined, text);
+      });
+
+      await clipboard.listenWithOptions({ includeOliveHelpsEvents: false }, callback);
+      expect(callback).toHaveBeenCalledWith(text);
+    });
+
+    it('rejects with the error when the underlying call throws an error', () => {
+      const exception = 'Exception';
+      mocked(oliveHelps.clipboard.listenAll).mockImplementation(() => {
+        throw exception;
+      });
+
+      const callback = jest.fn();
+      expect(() =>
+        clipboard.listenWithOptions({ includeOliveHelpsEvents: false }, callback),
+      ).rejects.toBe(exception);
     });
   });
 
