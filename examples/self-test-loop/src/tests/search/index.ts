a7eca276331198c2ea071d6c0a1ebe924ac5891d
Jeff Sun <jeff.sun@oliveai.com>
2022-06-15 13:11:17 -0400

    change create/exist index test to use fake data

--- examples/self-test-loop/src/tests/search/index.ts
+++ examples/self-test-loop/src/tests/search/index.ts
@@ -21,25 +21,13 @@ function workSheet2Document(worksheet: Worksheet) {
 
 export const testSearchCreateIndex = (): Promise<boolean> =>
   new Promise(async (resolve, reject) => {
-    let request = await network.httpRequest({
-      url: 'https://github.com/open-olive/loop-development-kit/raw/develop/examples/self-test-loop/static/test.xlsx',
-      method: 'GET',
-    });
-    let branch = 'develop';
-
-    // Above URL is used after this feature is finished and merged in
-    // Before PR is merged and branch is deleted, use the feature branch
-    // Can delete this block after merge
-    if (request.statusCode === 404) {
-      request = await network.httpRequest({
-        url: 'https://github.com/open-olive/loop-development-kit/raw/HELPS-4731-search-aptitude-selftest/examples/self-test-loop/static/test.xlsx',
-        method: 'GET',
-      });
-      branch = 'HELPS-4731-search-aptitude-selftest';
-    }
-    const workbook = await document.xlsxDecode(request.body);
-    const documents = workbook.worksheets.map(workSheet2Document);
-
+    const documents: search.Document[] = [
+      {
+        name: 'test',
+        data: JSON.stringify([]),
+        fields: [],
+      },
+    ];
     await whisper.create({
       label: 'Create index test',
       onClose: () => undefined,
@@ -50,7 +38,6 @@ export const testSearchCreateIndex = (): Promise<boolean> =>
         },
       ],
     });
-    console.log(JSON.stringify(documents));
     const index = await search.createIndex('testIndex', documents, {});
     if (index != null) {
       resolve(true);
@@ -89,8 +76,6 @@ export const testSearchCreateIndexSearch = (): Promise<boolean> =>
         },
       ],
     });
-
-    console.log(JSON.stringify(documents));
     const index = await search.createIndex('Test Index', documents, {});
     try {
       //console.log(JSON.stringify(index));
@@ -153,7 +138,6 @@ export const testSearchCreateIndexQueryStringSearch = (): Promise<boolean> =>
       ],
     });
 
-    console.log(JSON.stringify(documents));
     const index = await search.createIndex('Test Index', documents, {});
     try {
       //console.log(JSON.stringify(index));
@@ -186,25 +170,13 @@ export const testSearchCreateIndexQueryStringSearch = (): Promise<boolean> =>
 
 export const testSearchIndexExsit = (): Promise<boolean> =>
   new Promise(async (resolve, reject) => {
-    let request = await network.httpRequest({
-      url: 'https://github.com/open-olive/loop-development-kit/raw/develop/examples/self-test-loop/static/test.xlsx',
-      method: 'GET',
-    });
-    let branch = 'develop';
-
-    // Above URL is used after this feature is finished and merged in
-    // Before PR is merged and branch is deleted, use the feature branch
-    // Can delete this block after merge
-    if (request.statusCode === 404) {
-      request = await network.httpRequest({
-        url: 'https://github.com/open-olive/loop-development-kit/raw/HELPS-4731-search-aptitude-selftest/examples/self-test-loop/static/test.xlsx',
-        method: 'GET',
-      });
-      branch = 'HELPS-4731-search-aptitude-selftest';
-    }
-    const workbook = await document.xlsxDecode(request.body);
-    const documents = workbook.worksheets.map(workSheet2Document);
-
+    const documents: search.Document[] = [
+      {
+        name: 'test',
+        data: JSON.stringify([]),
+        fields: [],
+      },
+    ];
     await whisper.create({
       label: 'Search exist index test',
       onClose: () => undefined,
